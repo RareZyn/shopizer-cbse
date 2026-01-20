a302bb1f76b3b4801ff3f47a6b2590ae35169dd0
Razin <razinnazarudin@gmail.com>
2026-01-20 22:35:37 +0800

    Catalog done

--- shopizer-osgi/com.shopizer.common/src/main/java/com/shopizer/common/entity/Order.java
+++ shopizer-osgi/com.shopizer.common/src/main/java/com/shopizer/common/entity/Order.java
@@ -47,13 +47,25 @@ public class Order {
     @Column(nullable = false)
     private OrderStatus status;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "shipping_address_id")
-    private Address shippingAddress;
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "street", column = @Column(name = "shipping_street")),
+        @AttributeOverride(name = "city", column = @Column(name = "shipping_city")),
+        @AttributeOverride(name = "state", column = @Column(name = "shipping_state")),
+        @AttributeOverride(name = "country", column = @Column(name = "shipping_country")),
+        @AttributeOverride(name = "postalCode", column = @Column(name = "shipping_postal_code"))
+    })
+    private AddressInfo shippingAddress;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "billing_address_id")
-    private Address billingAddress;
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "street", column = @Column(name = "billing_street")),
+        @AttributeOverride(name = "city", column = @Column(name = "billing_city")),
+        @AttributeOverride(name = "state", column = @Column(name = "billing_state")),
+        @AttributeOverride(name = "country", column = @Column(name = "billing_country")),
+        @AttributeOverride(name = "postalCode", column = @Column(name = "billing_postal_code"))
+    })
+    private AddressInfo billingAddress;
 
     @Column(name = "shipping_method")
     private String shippingMethod;
